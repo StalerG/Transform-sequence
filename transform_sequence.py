07ba1ec91b03ee4d1cf400a16f7660be674b007e
Vadim <96094044+StalerG@users.noreply.github.com>
2026-03-18 19:46:17 +1000

    Update transform_sequence.py

--- transform_sequence.py
+++ transform_sequence.py
@@ -1,3 +1,5 @@
+from typing import Callable
+
 def transform_sequence(data: list, output_type: type = list, key: Callable = lambda obj: ' '.join(obj))-> list:
     """Универсальный преобразователь типов"""
     return output_type(map(key, data))
